f5dab0cd9e334d04cffe3f99973c9be43862030c
Akze <57077182+the-akze@users.noreply.github.com>
2025-09-27 22:14:49 -0400

    we don't need this web server for the hackathon; added a tiny bit of firebase code and route

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ const express = require("express");
 const expressApp = express();
 const port = 3001;
 const { initializeApp } = require('firebase-admin/app');
+const { getDatabase } = require("firebase-admin/database");
 
 require("dotenv").config();
 
 const fbApp = initializeApp(JSON.parse(process.env.FIREBASE_ADMIN_KEY));
+const database = getDatabase();
 
 console.log(fbApp);
 
 
 expressApp.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+expressApp.post("/auth/new-user", (req, res) => {
+  const userData = req.body;
+  let ref = database.ref(userData.path);
 });
 
 expressApp.listen(port, () => {
