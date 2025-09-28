8ffe5726e227016d6e684cdd3628c22d2d0201ce
Akze <57077182+the-akze@users.noreply.github.com>
2025-09-28 06:32:39 -0400

    backend is only being used as proxy for ical data

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require("express");
+const ical = require("node-ical");
+const cors = require("cors");
+
 const expressApp = express();
 const port = 3001;
-const { initializeApp } = require('firebase-admin/app');
-const { getDatabase } = require("firebase-admin/database");
 
-require("dotenv").config();
-
-const fbApp = initializeApp(JSON.parse(process.env.FIREBASE_ADMIN_KEY));
-const database = getDatabase();
-
-console.log(fbApp);
-
+expressApp.use(cors());
 
 expressApp.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-expressApp.post("/auth/new-user", (req, res) => {
-  const userData = req.body;
-  let ref = database.ref(userData.path);
+expressApp.get("/ical", (req, res) => {
+  let icalUrl = "https://calendar.google.com/calendar/ical/akze.alt%40gmail.com/public/basic.ics";
+  ical.async.fromURL(icalUrl).then(value => {
+    console.log(value);
+    res.json(value);
+  });
 });
 
 expressApp.listen(port, () => {
